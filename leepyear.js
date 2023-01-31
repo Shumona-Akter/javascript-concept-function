d2a04cc08df50907aaa6d3ca27784f1d9a49b7fc
SHumona-Akter <shumona.cse.bd@gmail.com>
2023-01-31 14:48:17 +0600

    leap year

---
+++ leepyear.js
@@ -0,0 +1,11 @@
+function myLeapYear(year){
+    if(year % 4 == 0){
+    return (" this is leep year")
+
+    }
+    return (" this is not a leep year")
+    
+}
+
+const myYear = myLeapYear(2010);
+console.log(myYear)
